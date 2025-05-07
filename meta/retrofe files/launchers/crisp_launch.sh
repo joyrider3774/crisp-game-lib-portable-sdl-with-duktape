9150434cb2a2352738287dfac5f0293539b6a7b5
willems davy <joyrider3774@hotmail.com>
2025-05-07 19:13:01 +0200

    update retrofe files/launchers/crisp_launch.sh

--- meta/retrofe files/launchers/crisp_launch.sh
+++ meta/retrofe files/launchers/crisp_launch.sh
@@ -2,7 +2,7 @@
 
 # Launch the process in background, record the PID into a file, wait
 # for the process to terminate and erase the recorded PID
-opkrun -m explorer.desktop "/mnt/Native games/Cglpsdl1.opk" "$1"&
+opkrun -m explorer.desktop "/mnt/Native games/Cglpsdl1-duk.opk" "$1"&
 pid record $!
 wait $!
 pid erase
